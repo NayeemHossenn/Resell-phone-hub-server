e172573680dc99bb9ad99b0519ec308caaf1037b
NayeemHossenn <nayeemhossen245@gmail.com.com>
2022-11-26 17:07:42 +0600

    Basic mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -7,6 +8,14 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbqmywm.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 app.get("/", (resq, res) => {
   res.send("resale phone erver is running");
