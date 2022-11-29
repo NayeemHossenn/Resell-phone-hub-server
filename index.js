62cb9fb77fbf8d0560e31068a119539c47e1cc03
NayeemHossenn <nayeemhossen245@gmail.com.com>
2022-11-30 01:23:52 +0600

    delete added

--- index.js
+++ index.js
@@ -131,6 +131,12 @@ async function run() {
       const result = await productCollection.find(query).toArray();
       res.send(result);
     });
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(filter);
+      res.send(result);
+    });
   } finally {
   }
 }
